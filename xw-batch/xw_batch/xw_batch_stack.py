55170968eca331976e6a731973d64689c2bd3a57
Jan Katins <jan.katins@kreuzwerker.de>
2022-08-09 21:35:41 +0200

    Add s3 bucket for raw data

--- xw-batch/xw_batch/xw_batch_stack.py
+++ xw-batch/xw_batch/xw_batch_stack.py
@@ -1,19 +1,19 @@
+import aws_cdk
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_s3,
 )
+
 from constructs import Construct
 
-class XwBatchStack(Stack):
 
+class XwBatchStack(aws_cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "XwBatchQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # s3 bucket for raw data
+        self.s3_raw_bucket = aws_s3.Bucket(
+            self,
+            id="xw_batch_bucket_raw",
+            # TODO: define a name in some config
+            # bucket_name="xw-batch-bucket-raw-" + os.environ("ENVIRONMENT_NAME"),
+        )
