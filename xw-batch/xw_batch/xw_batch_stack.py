a0a92dc9e2fc6562628e42d91d52ae8e6faaec65
Jan Katins <jan.katins@kreuzwerker.de>
2022-08-25 11:26:49 +0200

    Add User and Group management [DE-13]
    The idea is to have a construct which creates the whole user and group structure. It should be as easy as possible to add and remove users and groups.

--- xw-batch/xw_batch/xw_batch_stack.py
+++ xw-batch/xw_batch/xw_batch_stack.py
@@ -3,6 +3,11 @@ from aws_cdk import aws_glue, aws_iam, aws_s3
 from constructs import Construct
 
 from .copy_example_data import add_copy_scoofy_example_data
+from .users_and_groups import (
+    GROUP_DATA_LAKE_DEBUGGING,
+    OrgUsersAndGroups,
+    create_org_groups,
+)
 
 
 class XwBatchStack(aws_cdk.Stack):
@@ -38,6 +43,8 @@ class XwBatchStack(aws_cdk.Stack):
 
         # Add stuff for some example data
         example_data_location = add_copy_scoofy_example_data(self, self.s3_raw_bucket)
+
+        self.users_and_groups: OrgUsersAndGroups = create_org_groups(self)
 
         # Idea:
         # 1. "something" puts data into <bucket>/raw/source/table, crawler (per table) discovers it via cron
